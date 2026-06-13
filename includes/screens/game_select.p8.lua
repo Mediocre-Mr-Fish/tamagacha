723025981050a69bc5f51f7a367c7169ee8a25cd
Ellison Song <ellsong@ucdavis.edu>
2026-06-13 02:05:27 -0700

    minigames are now auto-detected

--- includes/screens/game_select.p8.lua
+++ includes/screens/game_select.p8.lua
@@ -1,37 +1,40 @@
 do
  screens.game_select = classfactory__gridmenu({
-  x = 8, y = 8, dx = 60, dy = 60, w = 2, h = 2,
-  selectables = {
-   { name = "math", key = "math" },
-   { name = "maze", key = "maze" },
-   { name = "fishing", key = "fishing" },
-   { name = "you shouldn't see this", key = nil }
-  }
+  x = 8, y = 8, dx = 60, w = 2, h = 2,
+  selectables = {}
  })
  local _ENV, scn = rescope(screens.game_select, _ENV)
+
+ for file in all(files_games) do
+  if is_cart(file) then
+   local grim = sub(file, 1, 1) == "_"
+   add(
+    selectables, {
+     name = sub(file, grim and 2 or 1, -4),
+     file = file,
+     grim = grim
+    }
+   )
+  end
+ end
+ assert(#selectables > 0, "no game carts found.")
+ h = (#selectables + 1) \ 2
+ dy = 120 \ h
+ panel_h = dy - 8
+
  function update()
   local _, game = update_sel(scn)
   glide(scn)
 
-  -- MARK: ToDo: whatever this is
-  local game4 = selectables[4]
-  if settings.grim then
-   -- game4.name = grim_progress .. "/3"
-   game4.name = "secret"
-   game4.key = "secret"
-   game4.col = nil
-  else
-   game4.name = "tbd"
-   game4.key = nil
-   game4.col = 5
-  end
-
   if btnp(🅾️) then
    switch_screen()
   elseif btnp(❎) then
-   if game.key then
-    load("games/" .. game.key .. ".p8", "exit")
-    load("games/" .. game.key .. ".p8.png", "exit")
+   if settings.grim or not game.grim then
+    local file = (IS_HTML and "" or "games/") .. game.file
+    load(file, "exit")
+    load(file .. ".png", "exit")
+   else
+    sfx(0)
    end
   end
  end
@@ -39,13 +42,14 @@ do
   fillp(█)
   for i, game in ipairs(selectables) do
    local x, y = grid_vec(scn, i):unpack()
+   local shown = (settings.grim or not game.grim)
 
-   draw_panel(game.name, x, y, 52, 52, game.col or 3)
+   draw_panel(shown and game.name or "n/a", x, y, 52, panel_h, shown and 3 or 5)
   end
-  rect_vec(sel_glider, vec2.new(52), 10, false, true)
+  rect_vec(sel_glider, vec2.new(52, panel_h), 10, false, true)
  end
  function draw_panel(label, x, y, w, h, col)
   rectfill(x, y, x + w, y + h, col)
-  print_centered(label, x + flr(w / 2), y + flr(h / 2) - 3, 7)
+  print_centered(label, x + w \ 2, y + h \ 2 - 3, 7)
  end
 end
